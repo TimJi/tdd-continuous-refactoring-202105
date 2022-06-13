8e09632d2956f550e3437f1dc4d2dc3f00a2de15
Tim Ji <timji@kktv.me>
2022-06-14 01:37:45 +0800

    [refactor] cleanup code

--- test/budgetService.js
+++ test/budgetService.js
@@ -9,10 +9,6 @@ export class BudgetService {
     if (endDay.isBefore(startDay)) {
       return 0
     }
-    if (startDay.isSame(endDay, 'month')) {
-      return (this.getAll()
-        ?.find(element => element.yearMonth === startDay.format('YYYYMM'))?.amount || 0) / startDay.daysInMonth() * (endDay.diff(startDay, 'day') + 1)
-    }
     let sum = 0
     let period = new Period(startDay, endDay);
     this.getAll()
@@ -20,16 +16,6 @@ export class BudgetService {
         let budget = Budget.from(b)
         sum += budget.dailyAmount() * period.overlappingDays(budget);
       })
-    // for (let currentMonth = startDay.startOf('month');
-    //   currentMonth.isBefore(endDay.add(1, 'month')
-    //     .startOf('month'));
-    //   currentMonth = currentMonth.add(1, 'month')) {
-    //   let budget = Budget.from(this.getAll()
-    //     ?.find(element => element.yearMonth === currentMonth.format('YYYYMM')));
-    //   if (budget) {
-    //     sum += budget.dailyAmount() * period.overlappingDays(budget)
-    //   }
-    // }
     return sum
   }
 }
